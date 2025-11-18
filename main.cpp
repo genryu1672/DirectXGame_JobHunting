f5bbe11eb54cb93ab53db998afb02185f038910b
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-11-18 15:58:00 +0900

    名前変更した

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace KamataEngine;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
 	////エンジンの初期化
-	KamataEngine::Initialize(L"LE3D_08_シマノ_ユウト_CG4");
+	KamataEngine::Initialize(L"LE3D_09_シマノ_ユウト_幽霊の紛争");
 	
 	//DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
